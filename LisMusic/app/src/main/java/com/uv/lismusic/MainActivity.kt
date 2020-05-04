637a44d44421233478936ea9ee133f164b5e89f3
Victor Niño <ninomtz.victor@gmail.com>
2020-05-04 14:25:37 -0500

    Navegación de  ListTracks a MediaPlayer

--- LisMusic/app/src/main/java/com/uv/lismusic/MainActivity.kt
+++ LisMusic/app/src/main/java/com/uv/lismusic/MainActivity.kt
@@ -1,39 +1,57 @@
 package com.uv.lismusic
 
 import android.content.Context
-import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.BaseAdapter
-import android.widget.ListView
-import android.widget.TextView
-import androidx.core.view.marginLeft
-import org.w3c.dom.Text
+import android.widget.*
+import android.widget.AdapterView.OnItemClickListener
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.collections.ArrayList
+
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val listView = findViewById<ListView>(R.id.main_listview)
-        listView.adapter = MyCustomAdapter(this)
+
+        val namesTracks = arrayListOf<String>(
+            "Soliá","Diles","Everyday","Good News","Heartless"
+        )
+        val namesArtist = arrayListOf<String>(
+            "Bad Bunny", "Rels B", "Logic", "Mac Miller", "The Weeknd"
+        )
+        listView.adapter = MyCustomAdapter(this, namesTracks, namesArtist)
+        listView.setOnItemClickListener { parent, view, position, id ->
+            goToPlayer(namesTracks[position], namesArtist[position])
+        }
+        
     }
 
-    private class MyCustomAdapter(context: Context): BaseAdapter(){
+    fun goToPlayer(track: String, artist:String): Unit {
+        val intent = Intent(this, player::class.java).apply {
+            putExtra("nameTrack",  track)
+            putExtra("nameArtist", artist)
+        }
+
+        this.startActivity(intent)
+    }
+
+    private class MyCustomAdapter(context: Context, tracks:ArrayList<String>, artist:ArrayList<String>): BaseAdapter(){
 
         private val mContext: Context
+        private val namesTracks: ArrayList<String>
+        private val namesArtist: ArrayList<String>
 
-        private val namesTracks = arrayListOf<String>(
-            "Soliá","Diles","Everyday","Good News","Heartless", "Canción 6", "Canción 7", "Canción 8","Canción 6", "Canción 7", "Canción 8"
-        )
-        private val namesArtist = arrayListOf<String>(
-            "Bad Bunny", "Rels B", "Logic", "Mac Miller", "The Weeknd", "Artista 6", "Artista 7", "Artista 8", "Artista 6", "Artista 7", "Artista 8"
-        )
         init {
             mContext = context
+            namesTracks = tracks
+            namesArtist = artist
         }
         override fun getView(position: Int, convertView: View?, viewGroup: ViewGroup?): View {
             val layoutInflater = LayoutInflater.from(mContext)
@@ -42,6 +60,9 @@ class MainActivity : AppCompatActivity() {
             val nameArtist = rowMain.findViewById<TextView>(R.id.artist_name)
             nameTrack.text = namesTracks.get(position)
             nameArtist.text = namesArtist.get(position)
+            val button = Button(mContext)
+            button.setText("Más")
+
             return rowMain
         }
 
