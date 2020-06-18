2dc90c1d5672e50c7eaaee9145caa58613962875
IvanGutru <47703419+IvanGutru@users.noreply.github.com>
2020-06-18 17:53:03 -0500

    Navegabilidad entre login y Home
    Se implementó el cierre de sesión con facebook y la validación de sesión iniciada

--- code/app/src/main/java/com/uv/lismusicjava/HomeActivity.java
+++ code/app/src/main/java/com/uv/lismusicjava/HomeActivity.java
@@ -1,7 +1,12 @@
 package com.uv.lismusicjava;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.facebook.AccessToken;
+import com.facebook.login.LoginManager;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,15 +21,32 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_search, R.id.navigation_library)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+
+        if(AccessToken.getCurrentAccessToken() == null){
+            goLoginScreen();
+        }else{
+            BottomNavigationView navView = findViewById(R.id.nav_view);
+            // Passing each menu ID as a set of Ids because each
+            // menu should be considered as top level destinations.
+            AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                    R.id.navigation_home, R.id.navigation_search, R.id.navigation_library)
+                    .build();
+            NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+            NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+            NavigationUI.setupWithNavController(navView, navController);
+
+        }
+    }
+
+    private void goLoginScreen() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    public void LogoutWithFacebook(View view){
+        LoginManager.getInstance().logOut();
+        goLoginScreen();
     }
 
 }
